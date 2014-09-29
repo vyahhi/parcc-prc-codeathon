d6622b13b3c1150bba6c12431daec2541b913d41
jfranks <franksjonathan@gmail.com>
2014-09-29 12:18:41 -0500

    2.5 still not working

---
+++ test/features/bootstrap/FeatureContext.php
@@ -0,0 +1,26 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+  Behat\Behat\Context\TranslatedContextInterface,
+  Behat\Behat\Context\BehatContext,
+  Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+  Behat\Gherkin\Node\TableNode;
+
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends \Drupal\DrupalExtension\ServiceContainer\DrupalExtension implements Context, SnippetAcceptingContext
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+}
