b827428f64f5c612e808c5c4f3d06e8d78e7d048
jfranks <franksjonathan@gmail.com>
2015-01-09 17:40:57 -0600

    PRC-476 Working through resuming tests

---
+++ docroot/sites/all/themes/prc_responsive_bartik/templates/quiz-report-form.tpl.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @file
+ * Themes the question report
+ *
+ */
+/*
+ * Available variables:
+ * $form - FAPI array
+ *
+ * All questions are in form[x] where x is an integer.
+ * Useful values:
+ * $form[x]['question'] - the question as a FAPI array(usually a form field of type "markup")
+ * $form[x]['score'] - the users score on the current question.(FAPI array usually of type "markup" or "textfield")
+ * $form[x]['max_score'] - the max score for the current question.(FAPI array of type "value")
+ * $form[x]['response'] - the users response, usually a FAPI array of type markup.
+ */
+?>
+<div class="quiz-report">
+
+  Score: <?php print $form[0]['score']['#value'] ?>.<br/>
+
+  <?php if ((int)$form[0]['score']['#value'] >= (int)$form[0]['passing_grade']['#value']) : ?>
+    Congratulations! You successfully passed the <?php print $form[0]['quiz_title']['#value'] ?> exam.
+  <?php else: ?>
+    Sorry. You did not reach the required score to pass the <?php print $form[0]['quiz_title']['#value'] ?> exam. Please try again later.<br/>
+    You may revisit the course modules and retake the course exam when ready.
+  <?php endif; ?>
+
+</div>
