ff1109fa697570d2952d756c148774d95497571f
jfranks <franksjonathan@gmail.com>
2015-02-06 11:26:40 -0600

    Taxonomy migration for simple vocabularies complete

--- docroot/sites/all/modules/custom/prc_migrate/src/PrcCsvSimpleTaxonomyMigration.php
+++ docroot/sites/all/modules/custom/prc_migrate/src/PrcCsvSimpleTaxonomyMigration.php
@@ -7,19 +7,17 @@
 
 class PrcCsvSimpleTaxonomyMigration extends Migration {
 
-  protected $vocab_name;
-
   /**
    * {@inheritdoc}
    */
   public function __construct($arguments = array()) {
     parent::__construct($arguments);
 
-    $this->vocab_name = $arguments['vocab_name'];
+    $vocab_name = $arguments['vocab_name'];
 
-    $this->description = t('Migrate @vocab_description terms from CSV to taxonomy terms', array('@vocab_description' => $this->vocab_name));
+    $this->description = t('Migrate @vocab_description terms from CSV to taxonomy terms', array('@vocab_description' => $vocab_name));
 
-    $path = drupal_get_path('module', 'prc_migrate') . '/data/' . $this->vocab_name . '.csv';
+    $path = drupal_get_path('module', 'prc_migrate') . '/data/' . $vocab_name . '.csv';
     $columns = array(
       0 => array('id', t('ID')),
       1 => array('name', t('Name')),
@@ -34,9 +32,9 @@ class PrcCsvSimpleTaxonomyMigration extends Migration {
 
     $this->source = new MigrateSourceCSV($path, $columns, $options, $fields);
 
-    $this->destination = new MigrateDestinationTerm($this->vocab_name);
+    $this->destination = new MigrateDestinationTerm($vocab_name);
 
-    $this->map = new MigrateSQLMap($this->vocab_name, self::getKeySchema(), $this->destination->getKeySchema());
+    $this->map = new MigrateSQLMap($vocab_name, self::getKeySchema(), $this->destination->getKeySchema());
 
     $this->addFieldMapping('name', 'name');
     $this->addFieldMapping('weight', 'weight');
