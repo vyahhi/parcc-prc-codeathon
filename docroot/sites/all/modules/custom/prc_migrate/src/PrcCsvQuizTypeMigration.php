8a2ae282c819569e5419d9af3d15338b5ea5cb8b
jfranks <franksjonathan@gmail.com>
2015-02-05 20:22:20 -0600

    Added Quiz Type term migration

---
+++ docroot/sites/all/modules/custom/prc_migrate/src/PrcCsvQuizTypeMigration.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Contains PrcCsvGenreMigration.
+ */
+
+class PrcCsvQuizTypeMigration extends PrcCsvSimpleTaxonomyMigration {
+
+  protected $vocab_name = 'quiz_type';
+
+}
