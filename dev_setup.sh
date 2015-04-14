3ab948aa57f7f936f4d78ee606410021e070550f
Jonathan Franks <jonathan.franks@breaktech.com>
2015-04-14 17:13:43 -0500

    Updated dev setup script for change to search index

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,6 +1,9 @@
 cd docroot
 drush si -y prc_profile
 drush fra -y --force
+drush dis -y prc_poc_local_search_api
+drush cc all
+drush en -y prc_poc_local_search_api --force
 drush mi --group=prc_taxonomy
 drush mi --group=prc_standard --limit="100 items"
 drush vocimp ../terms/lr_subjects_hier.csv tree --vocabulary_target=existing --vocabulary_id=10
