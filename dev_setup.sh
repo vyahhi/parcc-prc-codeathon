5e6cbc02ce40e1c1a93ca69dc87a6b09b66b54c0
Jonathan Franks <jonathan.franks@breaktech.com>
2015-05-06 10:07:19 -0500

    PRC-1420 Resource type changed subject vocab's tid from 10 to 11. Changed install script

--- dev_setup.sh
+++ dev_setup.sh
@@ -4,6 +4,6 @@ drush fra -y --force
 drush en -y prc_poc_local_search_api
 drush mi --group=prc_taxonomy
 drush mi --group=prc_standard --limit="100 items"
-drush vocimp ../terms/lr_subjects_hier.csv tree --vocabulary_target=existing --vocabulary_id=10
+drush vocimp ../terms/lr_subjects_hier.csv tree --vocabulary_target=existing --vocabulary_id=11
 drush mi --group=prc_content
 drush php-eval 'node_access_rebuild();'
